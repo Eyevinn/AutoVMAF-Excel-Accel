01f91580818b91612e6896013814f32797e0b3f0
Jonathan Walter <jonathan.walter@eyevinn.se>
2022-11-22 14:39:07 +0100

    feat: add positional args to get specific jobs

--- get-results.sh
+++ get-results.sh
@@ -3,9 +3,20 @@
 HOST="127.0.0.1"
 PORT="3000"
 
-for name in $(cat $(dirname $0)/jobnames.txt); do
-    echo Getting ${name} results...
-    curl -s "${HOST}:${PORT}/autoabr/result/${name}/" > results/${name}.json
-    echo Getting ${name} auto-ladder...
-    curl -s "${HOST}:${PORT}/autovmaf/ladder/${name}/" > results/${name}_ladder.json
-done
+if [[ $# -gt 0 ]]
+then
+    for name in "$@"
+    do
+        echo Getting ${name} results...
+        curl -s "${HOST}:${PORT}/autoabr/result/${name}/" > results/${name}.json
+        echo Getting ${name} auto-ladder...
+        curl -s "${HOST}:${PORT}/autovmaf/ladder/${name}/" > results/${name}_ladder.json
+    done
+else
+    for name in $(cat $(dirname $0)/jobnames.txt); do
+        echo Getting ${name} results...
+        curl -s "${HOST}:${PORT}/autoabr/result/${name}/" > results/${name}.json
+        echo Getting ${name} auto-ladder...
+        curl -s "${HOST}:${PORT}/autovmaf/ladder/${name}/" > results/${name}_ladder.json
+    done
+fi
